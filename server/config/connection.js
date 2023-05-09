39ac9a9f14fae919fe1b9d6040c91eac109894f2
Masyn N <masynnay33@gmail.com>
2023-05-08 20:39:14 -0600

    i don't know man

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+  process.env.MONGODB_URI || "https://masyn-book-search-engine.herokuapp.com/googlebooks"
 );
 
 module.exports = mongoose.connection;
