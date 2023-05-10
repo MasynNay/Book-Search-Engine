e092ba63dddf6b9ab8cf974d6dbd4727cf4f3ebc
Masyn N <masynnay33@gmail.com>
2023-05-09 22:24:48 -0600

    bug fixes

--- server/server.js
+++ server/server.js
@@ -7,19 +7,27 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const { authMiddleware } = require("./utils/auth");
 
-const server = new ApolloServer({
+const server = new ApolloServer({ // added
   typeDefs,
   resolvers,
-  context: ({ req }) => {
-    return {
-      user: req.user,
-    };
-  },
+  context: authMiddleware
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => { //added
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.use(authMiddleware);
-server.applyMiddleware({ app, context: ({ req }) => ({ req }) });
+const startApolloServer = async () => { //added
+  await server.start() //added
+  server.applyMiddleware({ app }) //adde
 
 db.once("open", () => {
   app.listen(PORT, () => {
@@ -28,3 +36,4 @@ db.once("open", () => {
     );
   });
 });
+}
